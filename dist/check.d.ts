aa64b3dc6921f6614742213df307959a500b35b2
Adam Mill <hismajesty@theroyalwhee.com>
2022-12-15 16:33:53 -0500

    Publish new version

---
+++ dist/check.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Is the value formatted as a sealed value.
+ * @param value The value to check.
+ * @returns True if formatted like a sealed value, otherwise false.
+ */
+export declare function isSealed(value: string): boolean;
--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.unseal = exports.seal = void 0;
+exports.isSealed = exports.unseal = exports.seal = void 0;
 var seal_1 = require("./seal");
 Object.defineProperty(exports, "seal", { enumerable: true, get: function () { return seal_1.seal; } });
 var unseal_1 = require("./unseal");
 Object.defineProperty(exports, "unseal", { enumerable: true, get: function () { return unseal_1.unseal; } });
+var check_1 = require("./check");
+Object.defineProperty(exports, "isSealed", { enumerable: true, get: function () { return check_1.isSealed; } });
 //# sourceMappingURL=index.js.map
